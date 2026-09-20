ca9b229c71d77018c83ab579004b41b3bbe0b400
winddpan <winddpan@gmail.com>
2026-09-20 16:05:19 +0800

    Initial Commit

---
+++ ImageAspectCropper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageAspectCropper
+//
+//  Created by winddpan on 2026/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageAspectCropper/ImageAspectCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAspectCropperApp.swift
+//  ImageAspectCropper
+//
+//  Created by winddpan on 2026/9/20.
+//
+
+import SwiftUI
+
+@main
+struct ImageAspectCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
